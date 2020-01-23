9999a616b66c473204073008ab7a8d9cad59e8a1
Denys <marzique@gmail.com>
2020-01-23 10:13:05 +0200

    more universal

--- python/list_to_triplets.py
+++ python/list_to_triplets.py
@@ -1,13 +1,14 @@
-def pack(_list):
-    """Split list into list of lists, each child list consists of 3 items"""
+def pack(_list, divider=3):
+    """
+    Split list into list of lists, each child list consists of 3 items by default.
+    """
     i = 0
     new_list = []
     while True:
-        mini_list = _list[i:i+3]
+        mini_list = _list[i:i+divider]
         if mini_list:
             new_list.append(mini_list)
         else:
             break
-        i += 3
+        i += divider
     return new_list
-
